edc3eafeb7595d9808ea572c455eda9791b8dbb5
Louis-David Coulombe <louis-david.coulombe@nesto.ca>
2023-12-04 21:55:36 -0500

    Solved day4 part 1

--- 2023/day04/main.go
+++ 2023/day04/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"sort"
 	"strings"
 
 	"github.com/louisdcoulombe/advent-of-code-go/cast"
@@ -38,20 +39,62 @@ func main() {
 	}
 }
 
+func sortedIntList(cards []string) []int {
+	list := []int{}
+	for _, c := range cards {
+		// Deal with 1 digit number in fixed double spacing
+		if len(c) == 0 {
+			continue
+		}
+		list = append(list, cast.ToInt(c))
+	}
+	sort.Ints(list)
+	return list
+}
+
+func gameCount(gameCards *[]int, handCards *[]int) int {
+	count := 0
+	for _, gc := range *gameCards {
+		for _, hc := range *handCards {
+			// Sorted list, if already greater it's not there
+			if hc > gc {
+				break
+			}
+
+			// Found same card
+			if hc == gc {
+				count += 1
+				break
+			}
+		}
+	}
+
+	if count == 0 {
+		return 0
+	}
+
+	return 1 << (count - 1)
+}
+
 func part1(input string) int {
 	parsed := parseInput(input)
-	_ = parsed
+	sum := 0
+	for _, line := range parsed {
+		// game card | your hand
+		parts := strings.Split(line, "|")
+		parts[0] = strings.TrimSpace(strings.Split(parts[0], ":")[1])
+		gameCards := sortedIntList(strings.Split(parts[0], " "))
+		handCards := sortedIntList(strings.Split(parts[1], " "))
+		sum += gameCount(&gameCards, &handCards)
+	}
 
-	return 0
+	return sum
 }
 
 func part2(input string) int {
 	return 0
 }
 
-func parseInput(input string) (ans []int) {
-	for _, line := range strings.Split(input, "\n") {
-		ans = append(ans, cast.ToInt(line))
-	}
-	return ans
+func parseInput(input string) (ans []string) {
+	return append(ans, strings.Split(input, "\n")...)
 }
